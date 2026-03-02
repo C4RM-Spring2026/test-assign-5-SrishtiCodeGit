1b77d6b69eff0a45b9ea86bc0f747b6423e8cc2d
Srishti Chauhan <sc5824@columbia.edu>
2026-03-02 00:38:58 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -2,13 +2,13 @@ import numpy as np
 
 def FizzBuzz(start, finish):
     nums = np.arange(start, finish + 1)
-    result = nums.astype(object)
+    out = np.array(nums, dtype = object)
+    mod3 = nums % 3 == 0
+    mod5 = nums % 5 == 0 
+    mod15 = nums % 15 == 0
+
+    out[mod3] = 'fizz'
+    out[mod5] = 'buzz'
+    out[mod15] = 'fizzbuzz'
     
-    fizz = nums % 3 == 0
-    buzz = nums % 5 == 0
-    
-    result[fizz & buzz] = "fizzbuzz"
-    result[fizz & ~buzz] = "fizz"
-    result[buzz & ~fizz] = "buzz"
-    
-    return result
+    return list(out)
